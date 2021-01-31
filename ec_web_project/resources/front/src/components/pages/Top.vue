1c01de27bcf65aab5b70e97b6cadd1b4bebade7e
duran_keiji <>
2021-01-31 16:31:08 +0900

    トップ画面にtest画像表示

--- ec_web_project/resources/front/src/components/pages/Top.vue
+++ ec_web_project/resources/front/src/components/pages/Top.vue
@@ -2,7 +2,22 @@
   <v-app>
     <v-main>
       <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center"></v-row>
+        <v-row align="center" justify="center">
+          <v-col v-for="n in 50" :key="n" class="d-flex child-flex" cols="3">
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
@@ -19,3 +34,10 @@ export default {
   }
 };
 </script>
+<style>
+.v-main {
+  padding-right: 7% !important;
+  padding-left: 7% !important;
+  margin-top: -53vh !important;
+}
+</style>
